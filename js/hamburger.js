189d090bffc91ad0cbac54a8703e853bb761c497
Daniel Lehre Ekeberg <danieleke@live.no>
2024-11-27 19:49:44 +0100

    Update hamburger.js

--- js/hamburger.js
+++ js/hamburger.js
@@ -6,19 +6,28 @@ hamburgerIcon.addEventListener('click', () => {
     dropdownMenu.classList.toggle('visible');
 });
 
+function updateActiveSort(activeId) {
+    const items = document.querySelectorAll('#dropdown-menu li');
+    items.forEach(item => item.classList.remove('active'));
 
+    const activeItem = document.getElementById(`sort-${activeId}`);
+    activeItem.classList.add('active');
+}
 
 document.getElementById('sort-newest').addEventListener('click', () => {
+    updateActiveSort('newest');
     sortPosts('newest');
     dropdownMenu.classList.remove('visible');
 });
 
 document.getElementById('sort-oldest').addEventListener('click', () => {
+    updateActiveSort('oldest');
     sortPosts('oldest');
     dropdownMenu.classList.remove('visible');
 });
 
 document.getElementById('sort-alphabetical').addEventListener('click', () => {
+    updateActiveSort('alphabetical');
     sortPosts('alphabetical');
     dropdownMenu.classList.remove('visible');
 })
